e189de97db147061f56934d5631110d68036e1ef
Rasoulmadani <ali.rahbary@chmail.ir>
2024-08-21 08:34:34 +0330

    ایجاد انکدر برای رمز

--- src/main/java/com/security/springSecurity/config/BasicConfiguration.java
+++ src/main/java/com/security/springSecurity/config/BasicConfiguration.java
@@ -7,16 +7,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class BasicConfiguration  {
+    @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
-        UserDetails users = User.withUsername("user").password("password").roles("USER").build();
+        UserDetails users = User.withUsername("user").password(passwordEncoder().encode("password")).roles("USER").build();
 
-        UserDetails admin = User.withUsername("admin").password("password").roles("ADMIN","USER").build();
+        UserDetails admin = User.withUsername("admin").password(passwordEncoder().encode("password")).roles("ADMIN","USER").build();
         return new InMemoryUserDetailsManager(users,admin);
     }
 
@@ -26,6 +29,11 @@ public class BasicConfiguration  {
                 request.anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder (){
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return encoder;
     }
 
 }
