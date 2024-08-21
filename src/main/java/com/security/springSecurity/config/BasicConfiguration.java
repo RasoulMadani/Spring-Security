2ab5e4680e997954ffcf22b5754f48818defd020
Rasoulmadani <ali.rahbary@chmail.ir>
2024-08-21 09:56:07 +0330

    کنترل اینکه کدام مسیر ها آزاد باشند و نیازی به وارد شدن به سایت نداشته باشند

--- src/main/java/com/security/springSecurity/config/BasicConfiguration.java
+++ src/main/java/com/security/springSecurity/config/BasicConfiguration.java
@@ -26,7 +26,7 @@ public class BasicConfiguration  {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception{
         return http.authorizeHttpRequests(request->
-                request.anyRequest().authenticated())
+                request.requestMatchers("/").permitAll().anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(form->form.loginPage("/login").permitAll())
                 .build();
