51e731990d9abe9f10db574e05cca335864b336e
Rasoulmadani <ali.rahbary@chmail.ir>
2024-08-21 13:11:15 +0330

    قرار دادن سطح دسترسی برای یک متد خاص

--- src/main/java/com/security/springSecurity/controller/UsersController.java
+++ src/main/java/com/security/springSecurity/controller/UsersController.java
@@ -1,6 +1,7 @@
 package com.security.springSecurity.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
     @GetMapping
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     public ResponseEntity<?> getAllUsers(){
         return ResponseEntity.ok("this is all users");
     }
