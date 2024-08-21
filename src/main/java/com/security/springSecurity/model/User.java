f42afff9ac4cb724dd2ff77cff5f0741699e4bb0
Rasoulmadani <ali.rahbary@chmail.ir>
2024-08-21 10:22:54 +0330

    ساختن مدل کاربر و ایجاد ریپازیتوری و سرویس برای آن

---
+++ src/main/java/com/security/springSecurity/model/User.java
@@ -0,0 +1,18 @@
+package com.security.springSecurity.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user_application")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String role;
+}
