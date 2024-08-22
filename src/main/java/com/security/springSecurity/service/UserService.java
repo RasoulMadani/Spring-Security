dfb778436921b8fe2f9ecba129e396fc1ad9d32f
Rasoulmadani <ali.rahbary@chmail.ir>
2024-08-22 12:09:13 +0330

    قرار دادن سطح دسترسی برای وقتی که متد چیزی که برمی گرداند با کاربر جاری یکی باشد

--- src/main/java/com/security/springSecurity/service/UserService.java
+++ src/main/java/com/security/springSecurity/service/UserService.java
@@ -3,6 +3,7 @@ package com.security.springSecurity.service;
 import com.security.springSecurity.model.User;
 import com.security.springSecurity.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,6 +24,7 @@ public class UserService implements UserDetailsService {
         return userRepository.findByUsername(username).orElseThrow(()->new RuntimeException("user.not.found"));
 
     }
+    @PostAuthorize("returnObject.username == authentication.principal.username")
     public User findById(int id){
         return userRepository.findById(id).orElseThrow(()->new RuntimeException("user.not.found"));
     }
