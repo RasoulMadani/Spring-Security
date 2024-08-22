6a0e1c61d4c01c52d5f85c09a4e6dc4b201cc3c0
Rasoulmadani <ali.rahbary@chmail.ir>
2024-08-22 10:01:21 +0330

    فرستادن اطلاعات به صفحه اچ تی ام ال و خواندن در آنجا

--- src/main/java/com/security/springSecurity/controller/IndexController.java
+++ src/main/java/com/security/springSecurity/controller/IndexController.java
@@ -1,6 +1,9 @@
 package com.security.springSecurity.controller;
 
+import com.security.springSecurity.model.User;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,5 +11,12 @@ public class IndexController {
     @GetMapping
     public String init(){
         return "index";
+    }
+    @GetMapping("/userDetails")
+    public String userDetails(Model model){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = (User) principal;
+        model.addAttribute("user",user);
+        return "userDetails";
     }
 }
