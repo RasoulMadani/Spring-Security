ecccbd34708265b7ce6b3f41d5702cc63fc34970
Rasoulmadani <ali.rahbary@chmail.ir>
2024-08-23 16:58:06 +0330

    اعتبار سنجی داده ها و برگداندن خطا ها به صفحه و نمایش آنها به کاربر

--- src/main/java/com/security/springSecurity/controller/LoginController.java
+++ src/main/java/com/security/springSecurity/controller/LoginController.java
@@ -1,24 +1,34 @@
 package com.security.springSecurity.controller;
 
 import com.security.springSecurity.dto.UserSaveRequest;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
     @GetMapping
-    public String loginPage(){
+    public String loginPage() {
         return "login";
     }
 
     @GetMapping("/register")
-    public String registerPage(){
+    public String registerPage(ModelMap modelMap) {
+        modelMap.put("userSaveRequest",new UserSaveRequest("1","1"));
         return "register";
     }
 
     @PostMapping("/register")
-    public String saveUser(@ModelAttribute UserSaveRequest userSaveRequest){
+    public String saveUser(
+            @ModelAttribute @Valid UserSaveRequest userSaveRequest
+            , BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
         System.out.println(userSaveRequest);
         return "redirect:/login";
     }
--- src/main/java/com/security/springSecurity/dto/UserSaveRequest.java
+++ src/main/java/com/security/springSecurity/dto/UserSaveRequest.java
@@ -1,7 +1,13 @@
 package com.security.springSecurity.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-public record UserSaveRequest(String username, String password) {
+public record  UserSaveRequest(
+        @NotEmpty @Size(min = 5) String username,
+        @NotEmpty @Size(min = 8) String password)
+{
+
 }
