18d479ef968ba52a81d529ed198b0b3703f8134a
Rasoulmadani <ali.rahbary@chmail.ir>
2024-08-24 13:22:20 +0330

    دیزیبل کردن سی اس آر اف با روش دیگر برای جلوگیری از قرار گرفتن این فیلد در فرم

--- src/main/java/com/security/springSecurity/config/BasicConfiguration.java
+++ src/main/java/com/security/springSecurity/config/BasicConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -36,7 +37,7 @@ public class BasicConfiguration  {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception{
-        return http.csrf().disable().authorizeHttpRequests(request->
+        return http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(request->
                 request.requestMatchers(HttpMethod.GET,"/","/index/products","/index/**","/login/register")
                         .permitAll()
 
